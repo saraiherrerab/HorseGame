ec41b4413c4a6d0aeb9860de099b81353c94184e
Sarai Gabriela <103385050+saraiherrerab@users.noreply.github.com>
2025-10-15 22:19:22 -0400

    Posición aleatoria y matriz

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -9,16 +9,72 @@ import androidx.appcompat.app.AppCompatActivity
 import android.graphics.Point
 import android.view.View
 import android.widget.TableRow
+import androidx.core.content.ContextCompat
+
 
 
 
 @Suppress("DEPRECATION")
 class MainActivity: AppCompatActivity() {
+    private var cellSelected_x = 0
+    private var cellSelected_y = 0
+
+    private lateinit var board: Array<IntArray>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initScreenGame()
+        resetBoard()
+        setFirstPosition()
+    }
+
+    private fun resetBoard(){
+        // 0 esta libre
+        // 1 casilla marcada
+        // 2 es un bonus
+        // 9 es una opcion del movimiento actual
+        board = arrayOf(
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0)
+        )
+    }
+
+    private fun setFirstPosition(){
+        var x = 0
+        var y = 0
+
+        x= (0..7).random()
+        y= (0..7).random()
+
+
+        cellSelected_x = x
+        cellSelected_y = y
+        selectCell(x, y)
+
+    }
+
+    private fun selectCell(x: Int, y: Int){
+
+        board[x][y] = 1
+        paintHorseCell(cellSelected_x, cellSelected_y, "previous_cell")
+        cellSelected_x = x
+        cellSelected_y = y
+        paintHorseCell(x, y, "selected_cell")
+
+    }
+
+    private fun paintHorseCell(x: Int, y: Int, color: String){
+        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id", packageName))
+        iv.setBackgroundColor(ContextCompat.getColor(this, resources.getIdentifier(color, "color", packageName)))
+        iv.setImageResource(R.drawable.icon)
     }
 
     private fun initScreenGame(){
